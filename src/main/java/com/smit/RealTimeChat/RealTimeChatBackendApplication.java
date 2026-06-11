0eef52c370c29931c8373733f76350dcf9cebd58
Smitpatel100 <smitptl5806@gmail.com>
2026-06-11 16:33:01 +0530

    DAY-01 Spring Boot setup

---
+++ src/main/java/com/smit/RealTimeChat/RealTimeChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smit.RealTimeChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smit/RealTimeChat/RealTimeChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smit.RealTimeChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
